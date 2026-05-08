e41f28972048ca762e27b6d49339d358dbbd52aa
Praize Okidi <praizeokidi@gmail.com>
2026-05-08 10:16:35 +0100

    new logo

--- src/components/Navbarr/Navbar.jsx
+++ src/components/Navbarr/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Dropdown from "./Dropdown";
-import Logo from "../../assets/rcmil.svg";
+import Logo from "../../assets/rcmix.svg";
 // import Button from "../Button";
 
 function Navbar() {
